127fbf1cbe7247bc549527dc2262289d475dbe8a
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-07-13 11:58:54 +0200

    Header Footer Container components created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Container from "../components/Container";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Container />
+      <Component {...pageProps} />
+      {/* Have to investigate why i can't wrap */}
+      {/* </Container> */}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {/* HEADER */}
+      {/* BODY */}
+      {/* FOOTER */}
     </div>
   );
 };
