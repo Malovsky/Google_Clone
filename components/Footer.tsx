9b194b0b61bb8dc407e5289aab4153660a0a34d8
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-07-19 11:34:34 +0200

    footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-4 text-sm text-gray-600">
+      Copyright &copy; {new Date().getFullYear()} Kevin MASLOWSKI
+    </footer>
+  );
 };
 
 export default Footer;
