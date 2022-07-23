991a7763e2cb43650a8a24bc5bda579c8bbfa7be
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-07-23 17:27:21 +0200

    added link to source code in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="flex absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-4 text-sm text-gray-600">
-      Copyright &copy; {new Date().getFullYear()} Kevin MASLOWSKI
+    <footer className="flex flex-col absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-4 text-sm text-gray-600">
+      <p>Copyright &copy; {new Date().getFullYear()} Kevin MASLOWSKI</p>
+      <p>
+        To see the source code, click{" "}
+        <Link href="https://github.com/Malovsky/Google_Clone">
+          <span className="text-blue-500 cursor-pointer">here</span>
+        </Link>
+        .
+      </p>
     </footer>
   );
 };
