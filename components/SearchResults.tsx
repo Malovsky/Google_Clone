e959fbba4327278b771dbeaa44522ec08c17983f
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-07-26 15:03:30 +0200

    added the result section and install html react parser

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import Parser from "html-react-parser";
+
 type SearchResultsProps = {
   // TYPE JSON ?
   results: any;
@@ -12,6 +14,24 @@ const SearchResults = ({ results }: SearchResultsProps) => {
         About {results.results.searchInformation.formattedTotalResults} results
         ({results.results.searchInformation.formattedSearchTime} seconds)
       </p>
+      {results.results.items.map((result: any) => (
+        <div key={result.link} className="max-w-xl mb-8">
+          <div className="group">
+            <a className="text-sm truncate" href={result.link}>
+              {result.formattedUrl}
+            </a>
+            <a
+              className="group-hover:underline decoration-blue-800"
+              href={result.link}
+            >
+              <h2 className="truncate text-xl font-medium text-blue-800">
+                {result.title}
+              </h2>
+            </a>
+          </div>
+          <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>
   );
 };
